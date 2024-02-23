12b75c9f83a4ebd809c6c24ee9454c1ab2ffb23f
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-02-23 21:23:03 +0100

    fix(DashboardController): IsGranted error

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -14,7 +14,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
-    #[IsGranted('ROLE_ADMIN')]
+    #[IsGranted('ROLE_ADMIN', statusCode: 404, message: 'Post not found')]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig');
