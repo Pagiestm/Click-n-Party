e15690316930a14dc8372338034205fb51cd15f3
pagiestm <pagiestm@gmail.com>
2024-02-29 22:35:31 +0100

    feat(HomeController.php, home/index.html.twig): retriveal and display of categories

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function Home(CategoriesRepository $categoriesRepo): Response
     {
+
+        $categories = $categoriesRepo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            "categories" => $categories,
         ]);
     }
 }
