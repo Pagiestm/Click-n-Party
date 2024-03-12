c34b0d8caa0bbb735a0c0a48cffd6e70ba1c3b07
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-03-12 20:42:52 +0100

    style(register.html.twig): add style for register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -22,14 +21,6 @@ class RegistrationFormType extends AbstractType
             ->add('Prenom')
             ->add('Telephone')
             ->add('Adresse')
-            ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
