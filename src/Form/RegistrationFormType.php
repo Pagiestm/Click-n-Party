d87bab3f8a680d21e2cf749b15573c0aa064b87b
pagiestm <pagiestm@gmail.com>
2024-03-30 00:12:02 +0100

    feat(RegistrationFormType.php): add recaptcha

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 class RegistrationFormType extends AbstractType
 {
@@ -22,7 +24,7 @@ class RegistrationFormType extends AbstractType
             ->add('Telephone')
             ->add('Adresse')
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
@@ -38,7 +40,10 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-        ;
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
