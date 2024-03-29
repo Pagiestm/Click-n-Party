9562c339d1f62915bafb3d08214f40147ae71a0a
pagiestm <pagiestm@gmail.com>
2024-03-29 16:37:36 +0100

    feat(ContactController.php): add MailerInterface

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function contact(Request $request, EntityManagerInterface $em): Response
+    public function contact(Request $request, EntityManagerInterface $em, MailerInterface $mailer): Response
     {
 
         $contact = new Contact();
@@ -33,6 +35,19 @@ class ContactController extends AbstractController
 
             $em->persist($contact);
             $em->flush();
+
+            $email = (new Email())
+                ->from('hello@hello.com')
+                ->to('admin@admin.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+
+            $mailer->send($email);
 
             $this->addFlash('success', 'Votre message a été envoyé avec succès');
 
