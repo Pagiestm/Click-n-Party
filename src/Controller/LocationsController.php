7d24ad9c836ff2f943460e5f18444314f82d9753
pagiestm <pagiestm@gmail.com>
2024-03-29 18:19:50 +0100

    fix(LocationsController.php): remove verif

--- src/Controller/LocationsController.php
+++ src/Controller/LocationsController.php
@@ -72,11 +72,6 @@ class LocationsController extends AbstractController
             $dateDebut = $reservation->getDateDebut();
             $dateFin = $reservation->getDateFin();
 
-            // Si la réservation est pour une seule journée, la date de fin doit être la même que la date de début
-            if ($dateDebut == $dateFin) {
-                $dateFin = clone $dateDebut;
-            }
-
             $nombreDeLocataires = $reservation->getNombresDeLocataires();
             $capaciteMaximale = $location->getCapaciteMaximal();
 
