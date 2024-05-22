280d7756783f518c8a3c1b6de14187fb5011e076
pagiestm <pagiestm@gmail.com>
2024-05-22 20:41:33 +0200

    feat(slideImage.js): infinity slide img

--- assets/scripts/slideImages.js
+++ assets/scripts/slideImages.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
+    loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
