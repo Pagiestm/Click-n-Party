938e323168694988d4018149677da9f4ed45e74a
pagiestm <pagiestm@gmail.com>
2024-06-01 14:23:45 +0200

    feat(filtreLocation, home): added text to indicate that there is no rental if a category is selected

--- assets/scripts/filtreLocations.js
+++ assets/scripts/filtreLocations.js
@@ -1,12 +1,25 @@
 document.querySelectorAll('.categorie').forEach(function(categorie) {
     categorie.addEventListener('click', function() {
         const categorieId = this.dataset.categorieId;
+        let hasVisibleLocation = false;
         document.querySelectorAll('.location').forEach(function(location) {
             if (location.dataset.categorieIds.split(',').includes(categorieId)) {
                 location.classList.remove('hidden');
+                hasVisibleLocation = true;
             } else {
                 location.classList.add('hidden');
             }
         });
+
+        const messageElement = document.getElementById('noLocationsMessage');
+        if (!hasVisibleLocation) {
+            if (messageElement) {
+                messageElement.classList.remove('hidden');
+            }
+        } else {
+            if (messageElement) {
+                messageElement.classList.add('hidden');
+            }
+        }
     });
 });
